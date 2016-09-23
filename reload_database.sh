4099b2c3642bfe10b7f0af6dfa4bb5d265994538
turbo <turbo@dubya>
2016-09-23 13:23:07 -0700

    changed reload script back to be user agnostic

--- reload_database.sh
+++ reload_database.sh
@@ -2,10 +2,10 @@
 # This script will delete the exchange database and then rebuild it fresh
 # from the schema.sql file.
 
-sudo -u postgres psql <<EOF
+psql <<EOF
 \x
 DROP DATABASE exchange;
 CREATE DATABASE exchange;
 EOF
 
-sudo -u postgres psql -f schema.sql exchange
+psql -f schema.sql exchange
