9b65ac358076e0566e0af0838b617c14964398d6
turbo <turbo@dubya>
2016-10-26 19:14:47 -0700

    fixed focus and added x button to modals

--- static/modal.js
+++ static/modal.js
@@ -2,6 +2,7 @@
 function buildHtml(content) {
     var html = `
         <div class="modal">
+           <div class="modal-close">×</div>
            <div class="modal-body">
            ${content}
            </div>
@@ -20,7 +21,15 @@ export function showModal(config) {
     var content = config.content || ''
     var $modal = $(buildHtml(content))    
     $('body').append($modal)
-    $('.modal-overlay').click(e => closeModal($modal)) 
+
+    // since we appended the modal the DOM is technically not ready for any more actions afterwards
+    // this seems kinda ghetto, can this be fixed?
+    $(document).ready(function(){
+        $modal.find("input:first").focus();
+    });
+
+    $('.modal-overlay').click(e => closeModal($modal))
+    $('.modal-close').click(e => closeModal($modal)) 
     return {
         $el: $modal,
         closeModal: () => { closeModal($modal) }
