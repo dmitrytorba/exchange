5c8479ba17596e4b49fb314914199d830cc073f6
turbo <turbo@dubya>
2016-11-28 23:21:30 -0800

    added redis rate limiting

--- api.go
+++ api.go
@@ -2,9 +2,14 @@ package main
 
 import (
 	_ "database/sql"
+	"fmt"
 	"github.com/gorilla/mux"
+	"gopkg.in/redis.v4"
 	"log"
 	"net/http"
+	"strconv"
+	"strings"
+	"time"
 )
 
 // route level error handling implemented at described in this article
@@ -24,11 +29,65 @@ func (fn appHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+// rateLimit is a function to be used by API paths to ensure no actor
+// can aggressively use the API. It expects a name of the feature being rate limited (i.e. "signup")
+// the request from which it will determine an ip address to limit, an optional username,
+// and the expiration time in seconds.
+func rateLimit(name string, r *http.Request, username string, exp int) (int64, error) {
+	ip := r.RemoteAddr
+	if r.Header.Get("X-Forwarded-For") != "" {
+		ips := strings.Split(r.Header.Get("X-Forwarded-For"), ",")
+		ip = ips[0]
+	}
+
+	key := fmt.Sprintf("%v:%v-%v", name, ip, username)
+	count, err := rd.Incr(key).Result()
+	if err != nil {
+		return 0, err
+	}
+
+	_, err = rd.Expire(key, time.Duration(exp)*time.Second).Result()
+	if err != nil {
+		return 0, err
+	}
+
+	fmt.Println(count)
+
+	return count, nil
+}
+
+// checkLimit will return where the user is currently at on rate limits
+func checkLimit(name string, r *http.Request, username string) (int64, error) {
+	ip := r.RemoteAddr
+	if r.Header.Get("X-Forwarded-For") != "" { // I should double check that this actually gets the ip
+		ips := strings.Split(r.Header.Get("X-Forwarded-For"), ",")
+		ip = ips[0]
+	}
+
+	fmt.Println("ip is", ip)
+
+	key := fmt.Sprintf("%v:%v-%v", name, ip, username)
+	count, err := rd.Get(key).Result()
+	if err == redis.Nil {
+		return 0, nil
+	}
+	if err != nil {
+		return 0, err
+	}
+
+	number, err := strconv.ParseInt(count, 10, 64)
+	if err != nil {
+		return 0, err
+	}
+
+	return number, nil
+}
+
 func api() (err error) {
 	router := mux.NewRouter().StrictSlash(true)
 
-	router.Handle("/bitfinex/trades/btcusd", appHandler(bitfinexTradesHandler)).Methods("GET")
-	
+	//router.Handle("/bitfinex/trades/btcusd", appHandler(bitfinexTradesHandler)).Methods("GET")
+
 	// order API
 	router.Handle("/order", appHandler(orderHandler)).Methods("POST") // creating buy/sell orders
 
--- api_signup.go
+++ api_signup.go
@@ -2,17 +2,40 @@ package main
 
 import (
 	"net/http"
+	"unicode/utf8"
 )
 
 func signupPost(w http.ResponseWriter, r *http.Request) error {
+
 	username := r.FormValue("username")
 	password := r.FormValue("password")
+	data := map[string]interface{}{
+		"Error":    "too many accounnts have been made by this ip address, please wait",
+		"Username": username,
+	}
+
+	// rate limit stuff
+	count, err := rateLimit("signup", r, "", 60*30)
+	if count > 1 { // cut-off for robots
+		data["Captcha"] = true
+	} else if count > 2 { // the 50 cut-off is to account for public places using our site
+		data["Error"] = "too many accounnts have been made by this ip address, please wait"
+		return executeTemplate(w, "signup", 200, data)
+	}
+
+	if utf8.RuneCountInString(username) == 0 || utf8.RuneCountInString(username) > 32 {
+		data["Error"] = "usernames must be between 0 and 32 characters"
+		return executeTemplate(w, "signup", 200, data)
+	}
 
 	if password != r.FormValue("password2") {
-		return executeTemplate(w, "signup", 200, map[string]interface{}{
-			"Error":    "passwords do not match",
-			"Username": username,
-		})
+		data["Error"] = "passwords do not match"
+		return executeTemplate(w, "signup", 200, data)
+	}
+
+	if utf8.RuneCountInString(password) < 3 || utf8.RuneCountInString(password) > 512 {
+		data["Error"] = "password needs to be between 3 and 512 characters"
+		return executeTemplate(w, "signup", 200, data)
 	}
 
 	user := &User{
@@ -20,13 +43,11 @@ func signupPost(w http.ResponseWriter, r *http.Request) error {
 		password: password,
 	}
 
-	err := createUser(user)
+	err = createUser(user)
 	if err != nil {
 		if err == ErrDuplicateUsername {
-			return executeTemplate(w, "signup", 200, map[string]interface{}{
-				"Error":    "username has been taken",
-				"Username": username,
-			})
+			data["Error"] = "username has been taken"
+			return executeTemplate(w, "signup", 200, data)
 		}
 
 		return err
@@ -38,5 +59,12 @@ func signupPost(w http.ResponseWriter, r *http.Request) error {
 }
 
 func signupHandler(w http.ResponseWriter, r *http.Request) error {
-	return executeTemplate(w, "signup", 200, nil)
+	count, err := checkLimit("signup", r, "")
+	if err != nil {
+		return err
+	}
+
+	return executeTemplate(w, "signup", 200, map[string]interface{}{
+		"Captcha": count > 1,
+	})
 }
